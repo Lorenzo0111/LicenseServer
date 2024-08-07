1a62255bf8013a0e836c9d68613e22b0b559d79e
Lorenzo <hello@lorenzo0111.me>
2024-08-07 17:55:26 +0200

    Added license create

--- src/app/licenses/page.tsx
+++ src/app/licenses/page.tsx
@@ -1,17 +1,37 @@
-import { License } from "@/components/dashboard/licenses/license";
+import {
+  CreateLicense,
+  License,
+} from "@/components/dashboard/licenses/license";
 import { prisma } from "@/lib/prisma";
 
 async function getLicenses() {
   return await prisma.license.findMany({});
 }
 
+async function getProducts() {
+  return await prisma.product.findMany({});
+}
+
+async function getUsers() {
+  return await prisma.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      name: true,
+    },
+  });
+}
+
 export default async function Products() {
   const licenses = await getLicenses();
+  const products = await getProducts();
+  const users = await getUsers();
 
   return (
     <main className="w-full p-4">
       <div className="mb-3 flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Licenses</h1>
+        <CreateLicense products={products} users={users} />
       </div>
 
       <div className="flex flex-wrap gap-3">
--- src/components/dashboard/licenses/license.tsx
+++ src/components/dashboard/licenses/license.tsx
@@ -2,6 +2,15 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import {
   Select,
@@ -11,9 +20,11 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useToast } from "@/components/ui/use-toast";
-import type { License as LicenseType } from "@prisma/client";
+import type { License as LicenseType, Product } from "@prisma/client";
 import axios from "axios";
+import { Plus } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export function License({ license }: { license: LicenseType }) {
   return (
@@ -24,6 +35,106 @@ export function License({ license }: { license: LicenseType }) {
         </CardHeader>
       </Card>
     </Link>
+  );
+}
+
+export function CreateLicense({
+  products,
+  users,
+}: {
+  products: Product[];
+  users: {
+    id: string;
+    name: string | null;
+    email: string | null;
+  }[];
+}) {
+  const { toast } = useToast();
+  const router = useRouter();
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus /> Add license
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create a new license</DialogTitle>
+          <DialogDescription>
+            Enter the license details to create it
+          </DialogDescription>
+        </DialogHeader>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            const data = new FormData(e.target as HTMLFormElement);
+            const product = data.get("product") as string;
+            const maxIps = data.get("maxIps") as string;
+            const expiresAt = data.get("expiresAt") as string | null;
+            const userId = data.get("userId") as string | null;
+
+            axios
+              .put(`/api/products/${product}/licenses`, {
+                maxIps: parseInt(maxIps),
+                expiresAt: expiresAt
+                  ? new Date(expiresAt).toISOString().split("T")[0]
+                  : undefined,
+                userId: userId || undefined,
+              })
+              .then((data) => {
+                toast({
+                  description: "License created",
+                });
+
+                router.push(`/licenses/${data.data.key}`);
+              })
+              .catch((error) => {
+                alert(error.response.data.error);
+              });
+          }}
+        >
+          <Select name="product" required>
+            <SelectTrigger className="mb-3 w-full">
+              <SelectValue placeholder="Product" />
+            </SelectTrigger>
+            <SelectContent>
+              {products.map((product) => (
+                <SelectItem key={product.id} value={product.id.toString()}>
+                  {product.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+
+          <div className="flex gap-3">
+            <Input type="number" name="maxIps" placeholder="Max Ips" />
+            <Input type="date" name="expiresAt" />
+          </div>
+
+          <Select name="userId">
+            <SelectTrigger className="mt-3 w-full">
+              <SelectValue placeholder="User" />
+            </SelectTrigger>
+            <SelectContent>
+              {users.map((user) => (
+                <SelectItem key={user.id} value={user.id}>
+                  {user.name} - {user.email}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+
+          <DialogFooter>
+            <Button className="mt-3" type="submit">
+              Submit
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
 }
 
